efe662fa09c0ba9f19eebac98ecd1dc8da755c47
stocksociety <arnold.kagunda@student.moringaschool.com>
2021-11-16 19:55:01 +0300

    change the files

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-alert("welcome to the akan's");
-
 function ghana(){
     var d=new Date();
     var day=document.getElementById("day").value;
@@ -14,90 +12,98 @@ if(day<0||day>31){
     alert("please fill in the field below correctly");
    
 }else{
-  d.setday(day);
+  d.setDate(day);
 }
-if(month<1 ||month>12){
+if(month <= 1 ||month>12){
 alert("please enter the correct month");
     
 }else if (month==""){
     alert("please fill the field below correctly");
     
 }else{
-   d.setmonth(month);
+   d.setMonth(month);
 }
-if(year<0|| year>2022){
+if(year<= 0|| year>2022){
  alert("please enter the correct year");
    
 }else if (year==""){
     alert("please fill the field below correctly");
    
 }else{
-  d.setyear(Year);
+  d.setYear(year);
 
 }
 
-var day=d.getDay();
-var days=["sunday", "monday","tuesday","wednesday","thursday","friday","saturday"]
-var male=["kwasi","kwadwo","kwabena","kwaku","Yaw","kofi","kwame"];
-var female=["akosua","Adwoa","abeena","Akua","Yaa","Afua","Ana"];
-//male functions
-function getghana(){
-    var ismale=document.getElementByid("male").checked;
-    var isfemale=document.getElementById("female").checked;
+var dayy = d.getDay();
+var days = ["sunday", "monday", "teusday", "wednesday", "thursday", "friday", "saturday"];
+var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var female = ["Akoswa", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-    if(ismale ==true && isfemale==false){
-        if(day==0){
-            alert("your Akan name is"+ male[0]);
+
+function getAkan(){
+    var ismale = document.getElementById("radiomale").checked;
+    var isfemale = document.getElementById("radiofemale").checked;
+
+    if(ismale == true && isfemale == false){
+
+        if(dayy == 0){
+            alert("Your Akan name is " +male[0]);
+        }
+        else if(dayy == 1){
+            alert("Your Akan name is " +male[1]);
+        }
+        else if(dayy == 2){
+            alert("Your Akan name is " +male[2]);
+        }
+        else if(dayy == 3){
+            alert("Your Akan name is " +male[3]);
+        }
+        else if(dayy == 4){
+            alert("Your Akan name is " +male[4]);
+        }
+        else if(dayy == 5){
+            alert("Your Akan name is " +male[5]);
+        }
+        else if(dayy == 6){
+            alert("Your Akan name is " +male[6]);
+        }
+        else{
+            alert("enter correct values");
+        }
+
+        
     }
-    else if(day==1){
-        alert("your Akan name is"+ male[1]);
-    }
-     else if(day==2) {
-        alert("your Akan name is"+ male[2]);
-    }
-     else if(day==3){
-        alert("your Akan name is"+ male[3]);
-     }
-      else if(day==4){
-        alert("your Akan name is"+ male[4]);
-      }
-    else if(day==5){
-        alert("your Akan name is"+ male[5]);
-      }
-    else if(day==6){
-        alert("your Akan name is"+ male[6]);
+    else if(ismale == false && isfemale == true){
+        
+        if(dayy == 0){
+            alert("Your Akan name is " +female[0]);
+        }
+        else if(dayy == 1){
+            alert("Your Akan name is " +female[1]);
+        }
+        else if(dayy == 2){
+            alert("Your Akan name is " +female[2]);
+        }
+        else if(dayy == 3){
+            alert("Your Akan name is " +female[3]);
+        }
+        else if(dayy == 4){
+            alert("Your Akan name is " +female[4]);
+        }
+        else if(dayy == 5){
+            alert("Your Akan name is " +female[5]);
+        }
+        else if(dayy == 6){
+            alert("Your Akan name is " +female[6]);
+        }
+        else{
+            alert("enter correct values");
+        }
+
     }
     else{
-        alert("enter valid values");}
+        alert("enter correct values");
     }
-//female functions
-else if(ismale==false && isfemale==true){
- if(day==0){
-        alert("your Akan name is"+ female[0]);
 }
-else if(day==1){
-    alert("your Akan name is"+ female[1]);
+getAkan();   
 }
-
-else if(day==2) {
-    alert("your Akan name is"+ female[2]);
-}
- else if(day==3){
-    alert("your Akan name is"+ female[3]);
- }
-  else if(day==4){
-    alert("your Akan name is"+ female[4]);
-  }
-else if(day==5){
-    alert("your Akan name is"+ female[5]);
-  }
-else if(day==6){
-    alert("your Akan name is"+ female[6]);
-}
-else{
-    alert("enter the valid values");}
-}
-}
- getghana();
-}
-
